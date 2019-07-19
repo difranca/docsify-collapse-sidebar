29ed8785b7c34ea673f30e69a70194c9a93e1689
difranca <difranca13@gmail.com>
2019-07-19 15:12:20 +0000

    Add support for path with spaces

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import styles from './styles.css'
 
 let collapse_sidebar = function(hook, vm) {
-    
+
     // Invoked each time after the data is fully loaded, no arguments
     hook.doneEach(function () {
 
@@ -10,7 +10,7 @@ let collapse_sidebar = function(hook, vm) {
         for (var i = 0; i < elems.length; i++) {
 
             let parent = elems[i];
-            
+
             // Ignores if the element has a link
             if (parent.children.length > 0) {
                 continue;
@@ -20,8 +20,9 @@ let collapse_sidebar = function(hook, vm) {
             let children = document.querySelector('.sidebar-nav > ul > li:nth-child(' + index + ') > ul');
 
             // Verifies if any child is opened
-            var current_url = RegExp("#.*").exec(decodeURI(window.location.href));
-            var is_opened = document.querySelector(".sidebar-nav > ul > li:nth-child(" + index + ") > ul [href='" + current_url + "']");
+            const current_url = RegExp("#.*").exec(window.location.href);
+            const query = ".sidebar-nav > ul > li:nth-child(" + index + ") > ul [href='" + current_url + "']"
+            const is_opened = document.querySelector(query)
 
             // Sets class for opened and closed items
             if (!is_opened) {
@@ -59,3 +60,4 @@ let collapse_sidebar = function(hook, vm) {
 }
 
 window.$docsify.plugins = [].concat(collapse_sidebar, window.$docsify.plugins)
+                                                                                
